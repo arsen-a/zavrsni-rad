427dec1e1d5f32e267e9b8a4f90163949399c565
arsen-a <arsen.s@live.com>
2019-07-30 03:33:57 +0200

    Zavrsen treci zadatak - hardkodovan

--- templates/allposts.php
+++ templates/allposts.php
@@ -1,5 +1,7 @@
 <div class="blog-post">
-    <h2 class="blog-post-title">Sample blog post</h2>
+    <a href="">
+        <h2 class="blog-post-title">Sample blog post</h2>
+    </a>
     <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
 
     <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
@@ -32,7 +34,9 @@
 </div><!-- /.blog-post -->
 
 <div class="blog-post">
-    <h2 class="blog-post-title">Another blog post</h2>
+    <a href="">
+        <h2 class="blog-post-title">Another blog post</h2>
+    </a>
     <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
 
     <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
@@ -44,7 +48,9 @@
 </div><!-- /.blog-post -->
 
 <div class="blog-post">
-    <h2 class="blog-post-title">New feature</h2>
+    <a href="">
+        <h2 class="blog-post-title">New feature</h2>
+    </a>
     <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
 
     <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean lacinia bibendum nulla sed consectetur. Etiam porta sem malesuada magna mollis euismod. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
